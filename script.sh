4a8b0823ac42326c2b262fd3a668f2fbcd6c6ff3
Gabriel Sistelos <gabrielsistelos@gmail.com>
2023-09-02 17:40:34 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y fish curl git ninja-build gettext cmake unzip
+sudo apt install -y fish curl git
 
 # Install mesloLGS font
 sudo mkdir -p /usr/share/fonts/truetype
@@ -12,18 +12,13 @@ curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fi
 fisher install IlanCosman/tide@v5
 END
 
-# Install and build neovim from source
-if [ ! -f /usr/local/bin/nvim ]; then
-	git clone https://github.com/neovim/neovim /tmp/neovim
-	make -C /tmp/neovim CMAKE_BUILD_TYPE=RelWithDebInfo
-	sudo make -C /tmp/neovim install
-fi
+chsh -s /usr/bin/fish
 
-if [ -f "/usr/local/bin/vim" ]; then
-	sudo rm /usr/local/bin/vim
-fi
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
 
-sudo ln -s /usr/local/bin/nvim /usr/local/bin/vim
+sudo mv nvim.appimage /usr/bin/nvim
+sudo ln -s /usr/bin/nvim /usr/bin/vim
 
 mkdir -p ~/.config
-cp -r nvim ~/.config/nvim
+git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim
