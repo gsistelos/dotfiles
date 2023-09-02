a4db73eee1ab96f9ab26485764803834013c29d8
gsistelos <gabrielsistelos@gmail.com>
2023-09-02 05:11:16 -0300

    changed fish config to be run as fish, no need to run script as sudo and nvim folder

--- script.sh
+++ script.sh
@@ -1,38 +1,25 @@
 #!/bin/sh
 
-# Check if if the script is being run with sudo
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run with sudo"
-    exit 1
-fi
+# Install dependencies
+sudo apt update
+sudo apt install -y fish curl git ninja-build gettext cmake unzip
 
 # Install mesloLGS font
-if [ ! -d /usr/share/fonts/truetype/mesloLGS ]; then
-	mkdir -p /usr/share/fonts/truetype
-	cp -r mesloLGS /usr/share/fonts/truetype
-fi
+sudo mkdir -p /usr/share/fonts/truetype
+sudo cp -r mesloLGS /usr/share/fonts/truetype
 
-# Install and configure fish
-if [ ! -f /usr/bin/fish ]; then
-	apt update
-	apt install -y fish curl
-
-	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-	fisher install IlanCosman/tide@v5
-fi
-
-# Install and configure neovim
+# Install and build neovim from source
 if [ ! -f /usr/local/bin/nvim ]; then
-	apt update -y
-	apt install -y git ninja-build gettext cmake unzip curl
-
-	git clone https://github.com/neovim/neovim ~/neovim
-	make -C ~/neovim CMAKE_BUILD_TYPE=RelWithDebInfo
-	make -C ~/neovim install
-
-	ln -s /usr/local/bin/nvim /usr/local/bin/vim
-
-	cp -r init.lua lua ~/.config/nvim
+	git clone https://github.com/neovim/neovim /tmp/neovim
+	make -C /tmp/neovim CMAKE_BUILD_TYPE=RelWithDebInfo
+	make -C /tmp/neovim install
 fi
 
+if [ -f "/usr/local/bin/vim" ]; then
+	sudo rm /usr/local/bin/vim
+fi
+
+sudo ln -s /usr/local/bin/nvim /usr/local/bin/vim
+
+mkdir -p ~/.config
+cp -r nvim ~/.config/nvim
