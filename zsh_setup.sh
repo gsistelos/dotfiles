71051b721cd80837a1f261a60708156cd4e04640
gsistelos <gabrielsistelos@gmail.com>
2023-11-14 02:47:16 -0300

    feat: unattended zsh_setup.sh; add: .zshrc

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt update && sudo apt upgrade
+sudo apt update
+sudo apt upgrade
 
-# For zsh and oh-my-zsh
 sudo apt install -y zsh curl git
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Install zsh-autosuggestions
+# zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Install zsh-syntax-highlighting
+# zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Zsh config
-cp .zshrc ~/.zshrc
+cp .zshrc ~/
