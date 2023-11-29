ebcd13fcb39482eee4cb880d042f5317528de02d
gsistelos <gabrielsistelos@gmail.com>
2023-11-29 12:42:22 -0300

    feat: new dependencies, clipboard and change vim-plug to lazyvim

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -11,14 +11,14 @@ chmod u+x nvim
 
 sudo mv nvim /usr/bin/
 
-# vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# plugins
 git clone https://github.com/gsistelos/nvim ~/.config/nvim
 
 # plugins dependencies
-sudo apt install -y gcc make nodejs
+sudo apt install -y gcc make nodejs npm unzip tar gzip fd-find
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.0.1/ripgrep_14.0.1-1_amd64.deb
 sudo dpkg -i ripgrep_14.0.1-1_amd64.deb
+
+# clipboard
+sudo apt install -y xclip
