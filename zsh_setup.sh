9666f9ac978455a2a321c972e24012882f0a3dd6
Gabriel Sistelos <gabrielsistelos@gmail.com>
2024-06-11 13:33:38 -0300

    zsh_setup.sh

---
+++ zsh_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# .zshrc
+curl -fsSL https://raw.githubusercontent.com/gsistelos/my-config/main/.zshrc -o ~/.zshrc
