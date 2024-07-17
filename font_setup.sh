eaaaf81057f6c02234c90e8391890ca691824155
gsistelos <gabrielsistelos@gmail.com>
2024-07-16 21:41:46 -0300

    changed font

--- font_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FONTS_PATH=/usr/share/fonts/TTF
-
-sudo curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o $FONTS_PATH/'MesloLGS NF Regular.ttf'
-sudo curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -o $FONTS_PATH/'MesloLGS NF Bold.ttf'
-sudo curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -o $FONTS_PATH/'MesloLGS NF Italic.ttf'
-sudo curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -o $FONTS_PATH/'MesloLGS NF Bold Italic.ttf'
