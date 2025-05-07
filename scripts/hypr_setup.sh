9cc0b8fffc8fbe950ee785d50d4ca2d2b1921a04
gsistelos <gsistelos@proton.me>
2025-05-07 05:28:23 +0000

    fix waybar font

--- scripts/hypr_setup.sh
+++ scripts/hypr_setup.sh
@@ -3,7 +3,7 @@
 source ./utils.sh
 
 require_pkg_manager
-require_packages "greetd-regreet uwsm hyprland hyprpaper xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland dunst noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra waybar ttf-ubuntu-nerd rofi alacritty ttf-jetbrains-mono-nerd"
+require_packages "greetd-regreet uwsm hyprland hyprpaper xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland dunst noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra waybar ttf-font-awesome rofi alacritty ttf-jetbrains-mono-nerd"
 
 FILES="$HOME/.config/hypr $HOME/.config/waybar $HOME/.config/rofi $HOME/.config/alacritty $HOME/.wallpapers"
 
