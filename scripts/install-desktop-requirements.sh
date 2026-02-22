0cb13d28717478268a55c02a7205fe8130b54fc4
Gabriel Sistelos <gsistelos@proton.me>
2026-02-21 22:17:05 -0300

    Add script to install desktop requirements

---
+++ scripts/install-desktop-requirements.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+pacman -S --needed \
+    hyprland \
+    foot \
+    ttf-meslo-nerd \
+    ttf-font-awesome \
+    noto-fonts \
+    noto-fonts-cjk \
+    noto-fonts-emoji \
+    noto-fonts-extra \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal-gtk \
+    qt5-wayland \
+    qt6-wayland \
+    qt5ct \
+    qt6ct \
+    adw-gtk-theme \
+    hyprlauncher \
+    hyprpaper \
+    grim \
+    slurp \
+    wl-clipboard
