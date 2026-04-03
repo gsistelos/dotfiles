69b540719f696502946e966400a74af89c89eca5
gsistelos <gsistelos@proton.me>
2026-04-03 16:51:59 -0300

    refactor: scripts

--- scripts/install-cli-requirements.sh
+++ scripts/install-cli-requirements.sh
@@ -1,40 +1,11 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-RED="\033[1;31m"
-GREEN="\033[1;32m"
-YELLOW="\033[1;33m"
-BLUE="\033[1;34m"
-RESET="\033[0m"
-
-SUCCESS="[${GREEN}SUCCESS${RESET}]"
-INFO="[${BLUE}INFO${RESET}]"
-WARNING="[${YELLOW}WARNING${RESET}]"
-ERROR="[${RED}ERROR${RESET}]"
-
-
-yn_question() {
-	local question="$1"
-
-	printf "%s [Y/n] " "${question}"
-	read -r answer
-
-	case ${answer} in
-		[yY] | [yY][eE][sS] | "")
-			return 0
-			;;
-		*)
-			return 1
-			;;
-	esac
-}
+source "$(dirname "$0")/utils.sh"
 
 
 check_distro() {
 	if [ ! -f /etc/os-release ]; then
-		echo "${ERROR} /etc/os-release is not a regular file"
-		exit 1
+		log_fatal "/etc/os-release is not a regular file"
 	fi
 
 	local distros="Arch Ubuntu Debian"
@@ -45,80 +16,81 @@ check_distro() {
 
 		if grep -q "^ID=${lowercase_distro}$" /etc/os-release; then
 			DISTRO="${distro}"
-			echo "${INFO} Running script for ${distro} distro..."
+			log_info "Running script for ${distro} distro..."
 		fi
 	done
 
 	if [ -z "${DISTRO:-}" ]; then
 		local distros_str
 		distros_str="$(echo "$distros" | sed -E 's/ /, /g; s/, ([^,]*)$/ and \1/')"
-		echo "${ERROR} This script only supports ${distros_str} distros"
-		exit 1
+		log_fatal "This script only supports ${distros_str} distros"
 	fi
 
 	if ! yn_question "Continue?"; then
+		log_info "Exiting..."
 		exit 0
 	fi
 }
 
 
 install_packages() {
-	local update_packages
-	local install_packages
+	local common_packages="git less curl unzip make bash zsh tmux"
+
+	local update_command
+	local install_command
 
 	if [ "${DISTRO}" = "Arch" ]; then
-		update_packages="sudo pacman -Syu --noconfirm"
-		install_packages="sudo pacman -S --needed --noconfirm git less curl unzip make bash zsh tmux"
+		local pacman_packages="${common_packages}"
+		update_command="sudo pacman -Syu --noconfirm"
+		install_command="sudo pacman -S --needed --noconfirm ${pacman_packages}"
 	elif [ "$DISTRO" = "Ubuntu" ] || [ "$DISTRO" = "Debian" ]; then
-		update_packages="sudo apt update && sudo apt upgrade -y"
-		install_packages="sudo apt install -y git less curl unzip make bash zsh tmux"
+		local apt_packages="${common_packages}"
+		update_command="sudo apt update && sudo apt upgrade -y"
+		install_command="sudo apt install -y ${apt_packages}"
 	fi
 
-	echo "${INFO} Updating packages..."
-	if ! eval "${update_packages}"; then
-		echo "${ERROR} Failed to update packages"
-		exit 1
+	log_info "Updating packages..."
+	if ! eval "${update_command}"; then
+		log_fatal "Failed to update packages"
 	fi
 
-	echo "${INFO} Installing packages..."
-	if ! eval "${install_packages}"; then
-		echo "${ERROR} Failed to install packages"
-		exit 1
+	log_info "Installing packages..."
+	if ! eval "${install_command}"; then
+		log_fatal "Failed to install packages"
 	fi
 }
 
 
 install_zsh_plugins() {
-	echo "${INFO} Installing zsh plugins..."
+	log_info "Installing zsh plugins..."
 
-	ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 	if [ ! -d "$HOME/.oh-my-zsh" ]; then
 		local install_script
 		install_script="$(mktemp)"
 		if ! curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o "${install_script}"; then
-			echo "${ERROR} Failed to download oh-my-zsh installer"
 			rm -f "${install_script}"
-			exit 1
+			log_fatal "Failed to download oh-my-zsh installer"
 		fi
 
 		if ! sh "${install_script}" --unattended --keep-zshrc; then
-			echo "${ERROR} Failed to install oh-my-zsh"
 			rm -f "${install_script}"
-			exit 1
+			log_fatal "Failed to install oh-my-zsh"
 		fi
 		rm -f "${install_script}"
 	else
-		echo "${INFO} oh-my-zsh already installed, skipping..."
+		log_info "oh-my-zsh already installed, skipping..."
 	fi
 
-	if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && ! git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"; then
-		echo "${ERROR} Failed to install zsh-autosuggestions"
-		exit 1
+	ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+	ZSH_AUTOSUGGESTIONS="${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+	if [ ! -d "${ZSH_AUTOSUGGESTIONS}" ] && ! git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_AUTOSUGGESTIONS}"; then
+		log_fatal "Failed to install zsh-autosuggestions"
 	fi
 
-	if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] && ! git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"; then
-		echo "${ERROR} Failed to install zsh-syntax-highlighting"
-		exit 1
+	ZSH_SYNTAX_HIGHLIGHTING="${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+	if [ ! -d "${ZSH_SYNTAX_HIGHLIGHTING}" ] && ! git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_SYNTAX_HIGHLIGHTING}"; then
+		log_fatal "Failed to install zsh-syntax-highlighting"
 	fi
 }
 
---
+++ scripts/utils.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+RESET="\033[0m"
+
+SUCCESS="[${GREEN}SUCCESS${RESET}]"
+INFO="[${BLUE}INFO${RESET}]"
+WARNING="[${YELLOW}WARNING${RESET}]"
+ERROR="[${RED}ERROR${RESET}]"
+
+
+yn_question() {
+	local question="$1"
+
+	printf "%s [Y/n] " "${question}"
+	read -r answer
+
+	case ${answer} in
+		[yY] | [yY][eE][sS] | "")
+			return 0
+			;;
+		*)
+			return 1
+			;;
+	esac
+}
+
+log_success() {
+    local message="$1"
+    printf "%s %s\n" "${SUCCESS}" "${message}"
+}
+
+log_info() {
+	local message="$1"
+	printf "%s %s\n" "${INFO}" "${message}"
+}
+
+log_warning() {
+	local message="$1"
+	printf "%s %s\n" "${WARNING}" "${message}"
+}
+
+log_error() {
+	local message="$1"
+	printf "%s %s\n" "${ERROR}" "${message}" >&2
+}
+
+log_fatal() {
+	local message="$1"
+	printf "%s %s\n" "${ERROR}" "${message}" >&2
+	exit 1
+}
