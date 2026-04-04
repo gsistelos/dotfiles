6204d1d4754bcb1c92db4b62ebc988f1e7beacf0
gsistelos <gsistelos@proton.me>
2026-04-03 23:00:41 -0300

    feat: disable remember signon

--- scripts/user.js
+++ scripts/user.js
@@ -1,5 +1,6 @@
 user_pref("browser.aboutwelcome.enabled", false);
 user_pref("browser.formfill.enable", false);
+user_pref("signon.rememberSignons", false);
 user_pref("places.history.enabled", false);
 user_pref("privacy.globalprivacycontrol.enabled", true);
 user_pref("privacy.globalprivacycontrol.was_ever_enabled", true);
